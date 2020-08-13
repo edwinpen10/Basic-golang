f9deef53b09ed6f618f0f6b4359bd7798a270d6d
@edwinpen0101 <edwinpen010@gmail.com>
2020-08-13 14:36:49 +0700

    append dan copy

--- slice.go
+++ slice.go
@@ -10,4 +10,21 @@ func main()  {
 
 	fmt.Println(member)
 	fmt.Println(sliceMembebr)
+
+	fmt.Println(len(sliceMembebr)) //menampilkan panjang array
+	fmt.Println(cap(sliceMembebr)) //menampilkan kapasistas didalam array
+
+	human := make([]string, 5, 10)
+	
+	fmt.Println(human)
+	fmt.Println(len(human))
+	fmt.Println(cap(human))
+
+	
+	humanity :=  make([]string, 5)
+	copy(humanity, sliceMembebr)
+
+	humanity = append(humanity,"jiraya","orocimaru")
+
+	fmt.Println(humanity)
 }
