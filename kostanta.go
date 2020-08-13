382917610eb965f73dc1f7b221af091329b8f9c7
@edwinpen0101 <edwinpen010@gmail.com>
2020-08-13 13:32:36 +0700

    update kosntanta

--- kostanta.go
+++ kostanta.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import ("fmt")
 
 func main()  {
 	const Name string ="Edwin pen"
+	const Namaku  ="Edwin"
+
+	const (
+		umur = 23
+		sekolah = "SMKN 1 Pangkalanbun"
+	)
+
+
 	fmt.Println(Name)
+	fmt.Println(Namaku)
+	fmt.Println(umur)
+	fmt.Println(sekolah)
 }
