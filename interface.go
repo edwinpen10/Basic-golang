1f020dc84c2dc6b1a28ffb2b37440709a400d28f
@edwinpen0101 <edwinpen010@gmail.com>
2020-08-24 12:59:59 +0700

    add method and interfeace update

--- interface.go
+++ interface.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"fmt"
+	"math"
 )
 
 type Bentuk interface{
@@ -21,10 +22,24 @@ func (pj persegipjg) luas() float64 {
 	return pj.panjang * pj.lebar
 }
 
+type lingkaran struct{
+	radius float64
+}
+
+func (l lingkaran) keliling() float64 {
+	return math.Pi * l.radius * l.radius
+}
+
+func (l lingkaran) luas() float64  {
+	
+	return 2 * math.Pi * l.radius
+}
 
 func main()  {
 	kotak1 := persegipjg{5,10}
 	hitungbangunan(kotak1)
+	lingkaran1 := lingkaran{10}
+	hitungbangunan(lingkaran1) 
 }
 
 func hitungbangunan(b Bentuk)  {
